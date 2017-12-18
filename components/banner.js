ff024124f35c26bef2df0249d70f2d4958b6987b
young <yang10177101@163.com>
2017-12-18 22:52:24 +0800

    banner

---
+++ components/banner.js
@@ -0,0 +1,109 @@
+import React from 'react';
+
+class Banner extends React.Component {
+	render() {
+		return(
+			<div id="banner">
+			<div className="ban-con">
+				<div className="slh">
+					<img src="https://img14.360buyimg.com/da/jfs/t14584/6/171502270/50219/f1e459f9/5a2573acNd913ebfc.png" />
+				</div>
+				<div className="ban-list">
+				<ul>
+					<li>
+						<a href="#">家用电器</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+					<li>
+						<a href="#">手机</a>/
+						<a href="#">运营商</a>/<a href="#">数码</a>
+					</li>
+				</ul>
+			</div>
+			<div className="ban-ban">
+				<div>
+					<img src="https://img14.360buyimg.com/babel/jfs/t16204/216/364955443/69591/10cf819/5a2fa3afNe9556f34.jpg" />
+				</div>
+				
+				<div className="ban-down">
+					<img src="https://img10.360buyimg.com/babel/s390x130_jfs/t12688/101/1608148615/20113/dd22654b/5a24f1d1N782b3833.jpg!q90" />
+					<img src="https://img11.360buyimg.com/babel/s390x130_jfs/t12172/290/2016201247/13278/239437d/5a2f71f1Ndb55ca71.jpg!q90" />
+				</div>
+			</div>
+			<div className="ban-login">
+				
+			</div>
+			</div>
+			<div className="aside">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+			<div className="to-top">
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+		)
+	}
+}
+
+export default Banner;
--- entry.js
+++ entry.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom'
 
 import './style/reset.css'
 import './style/find.css'
+import './style/banner.css'
 
 import Find from './components/find.js'
+import Banner from './components/banner.js'
+
 class App extends React.Component{
 	render(){
 		return(
 			<div>
 				<Find/>
+				<Banner/>
 			</div>
 		)
 	}
