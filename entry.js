fba30419a88056b0160f455976aa0beb5045acd8
yunkun <1175346450@qq.com>
2017-12-18 22:58:10 +0800

    nav ad

--- entry.js
+++ entry.js
@@ -4,16 +4,22 @@ import ReactDOM from 'react-dom'
 import './style/reset.css'
 import './style/find.css'
 import './style/banner.css'
+import './style/nav.css'
+import './style/ad.css'
 
 import Find from './components/find.js'
 import Banner from './components/banner.js'
+import Nav from './components/nav.js'
+import Ad from './components/ad.js'
 
 class App extends React.Component{
 	render(){
 		return(
 			<div>
-				<Find/>
+				<Ad/>
+				<Nav/>
 				<Banner/>
+				<Find/>
 			</div>
 		)
 	}
